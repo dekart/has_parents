e0220e306330abcb7304ce627b28f38a5415d8d5
DEkart <dekart@Lamptop.(none)>
2008-09-12 18:31:42 +0600

    Defining explicit path to required has_parents file

--- init.rb
+++ init.rb
@@ -1,3 +1,3 @@
-require "action_controller/has_parents"
+require File.join(File.dirname(__FILE__), "action_controller", "has_parents")
 
 ActionController::Base.send(:include, ActionController::HasParents)
